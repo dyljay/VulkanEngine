64a113692adde2c9353c00ac9ae10b0767c30dc2
dyljay <djayasundera3@gatech.edu>
2026-03-02 19:29:14 -0500

    fixing typo

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -2,5 +2,5 @@
 /opt/homebrew/bin/glslang -V shaders/shader.frag -o shaders/frag.spv
 /opt/homebrew/bin/glslang -V shaders/point_light.vert -o shaders/point_light.vert.spv
 /opt/homebrew/bin/glslang -V shaders/point_light.frag -o shaders/point_light.frag.spv
-/opt/homebrew/bin/glslang -V shaders/cubemap_vert.vert -o shaders/cubemap_vert.frag.spv
+/opt/homebrew/bin/glslang -V shaders/cubemap_vert.vert -o shaders/cubemap_vert.vert.spv
 /opt/homebrew/bin/glslang -V shaders/cubemap_frag.frag -o shaders/cubemap_frag.frag.spv
